e745ea6f762b74b3661f557f0671d0d34972aceb
KMyers06 <kmyers20@uwyo.edu>
2024-10-16 21:45:00 +0000

    Completed project

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Koen Myers
 # UWYO COSC 1010
-# Submission Date
+# 10/16/24
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 15
+# Sources, people worked with, help given to: Michael Salcidl, Emmanual
 # your
 # comments
 # here
@@ -78,8 +78,15 @@ print(len(random_string)) # Print out the size for reference
 # That is to say, this is a great use of dictionaries
     # You will  need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
+alphabet_acur={}
+for random in random_string:
+    if random not in alphabet_acur.keys():
+        alphabet_acur[random]=1
+    else:
+        alphabet_acur[random]+=1
 
-
+for alpha in sorted(alphabet_acur.keys()):
+    print(alpha,":",alphabet_acur[alpha])
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
 
@@ -88,8 +95,17 @@ print(len(random_string)) # Print out the size for reference
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+most_occurred = "a"
+most_number=0
+least_occurred = "a"
+least_number=25000
+for key in alphabet_acur.keys():
+    if alphabet_acur[key]>most_number:
+        most_occurred=key
+        most_number=alphabet_acur[key]
+    if alphabet_acur[key]<least_number:
+        least_occurred=key
+        least_number=alphabet_acur[key]
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
@@ -98,3 +114,5 @@ print(f"The letter that occurred the most is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+for al in sorted(alphabet_acur.keys()):
+    print(al,':',alphabet_acur[al]/25)
